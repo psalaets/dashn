dac9b33d4aa01bb3591b6cf71008d6cf2a0cf7f6
Paul Salaets <psalaets@gmail.com>
2013-12-07 17:47:12 -0500

    extra empty string if input ends with newline

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var dashn = require('..');
 
 module.exports['dashn'] = {
-  "given a function, sends it input line by line": function(test) {
+  "sends function input line by line": function(test) {
     var actual = [];
 
     var d = dashn(function(data) {
@@ -11,6 +11,18 @@ module.exports['dashn'] = {
     d.end("a\nb\nc");
 
     test.deepEqual(actual, ['a', 'b', 'c']);
+    test.done();
+  },
+  "last data chunk is empty string when input ends with newline": function(test) {
+    var actual = [];
+
+    var d = dashn(function(data) {
+      actual.push(data.toString());
+    });
+
+    d.end("a\nb\nc\n");
+
+    test.deepEqual(actual, ['a', 'b', 'c', '']);
     test.done();
   }
 };
