6ba5981c05d3e6b973c34c2b637b6b80eab9557b
Paul Salaets <psalaets@gmail.com>
2013-12-07 17:47:22 -0500

    rename

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var es = require('event-stream');
 
-module.exports = function dashn(expression) {
+module.exports = function dashn(writeFn) {
   return es.pipeline(
     es.split(),
-    es.through(expression)
+    es.through(writeFn)
   );
 };
